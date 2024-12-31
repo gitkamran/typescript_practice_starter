3dbd0f49f52f546dc3e11b8991cb8a6253af2793
gitkamran <mkamran.ir@gmail.com>
2025-01-01 01:25:12 +0330

    upload project

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = "Mohammad";
+console.log(username);
+let a = 16;
+let b = "2";
+let c = 8;
+console.log(a / c);
+console.log(a - c);
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+let username = "Mohammad";
+console.log(username);
+
+let a: number = 16;
+let b: string = "2";
+let c: number = 8;
+
+console.log(a / c);
+console.log(a - c);
